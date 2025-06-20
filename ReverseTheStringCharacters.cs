368c7962de6edc381bbc492b51334990702cc9da
Deepak Lohar <deepaklohar1993@gmail.com>
2025-06-20 17:51:11 +0530

    Update ReverseTheStringCharacters.cs

--- ReverseTheStringCharacters.cs
+++ ReverseTheStringCharacters.cs
@@ -1,1 +1,21 @@
+/*
+Reverse the character of string.
 
+if "Deepak" is string then output would be "kapeed"
+*/
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string Name = "Deepak";
+        char[] reverse = Name.ToCharArray();
+        for(int i = 0,j=Name.Length-1;i<j;i++,j--)
+        {
+            reverse[i] = Name[j];
+            reverse[j] = Name[i];
+        }
+        Console.WriteLine(new string(reverse));
+    }
+}
