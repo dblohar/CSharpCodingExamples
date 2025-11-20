86cd2319a3024aedfca69291f0745cbb3c9020df
Deepak Lohar <deepaklohar1993@gmail.com>
2025-11-20 13:16:52 +0530

    Implement second prime number checking method
    Added a second method for checking prime numbers and updated the main method to demonstrate its usage.

--- PrimeNumberMethod.cs
+++ PrimeNumberMethod.cs
@@ -1,4 +1,5 @@
- public static bool IsPrime(int num){
+/* METHOD 1 */
+public static bool IsPrime(int num){
         if(num<=1)
             return false;
         for(int i=2;i*i<=num;i++)
@@ -8,3 +9,37 @@
         }
         return true;
     }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*    METHOD 2   */
+
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       int num = 100;
+       Console.WriteLine($"Is {num} prime number? => {IsPrime(num)}");
+    }
+    public static bool IsPrime(int num)
+    {
+        if(num<=1)
+            return false;
+        if(num==2)
+            return true;
+        if(num % 2 == 0)
+            return false;
+        int limit = (int)Math.Sqrt(num);
+        
+        for(int i = 3;i< limit; i += 2)
+        {
+            if(num%i==0)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
